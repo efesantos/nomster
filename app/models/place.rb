bf1aa9a9aba698e1ace8a29879b2905de14d1ac5
Elmer <efe_santos@hotmail.com>
2016-03-17 16:42:05 +0000

    Create table for comments. Make association between Comment and the User and Place models

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,6 +3,7 @@ class Place < ActiveRecord::Base
   paginates_per 5 #Defined in Kaminati gem
 
   belongs_to :user
+  has_many :comments
 
   validates :name, :address, :description, presence: true
   validates :name, length: {minimum: 4}
