1976f1cc26dedb48a56b8ea6de8d7e82ee3af67c
Elmer <efe_santos@hotmail.com>
2016-03-21 14:33:34 +0000

    Configure app so that it can send real emails from a purposelly created gmail account

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "efe_santos@hotmail.com", subject: "A comment has been posted")
+    mail(to: "efesantos.webapps@gmail.com", subject: "A comment has been posted")
   end
 
 end
