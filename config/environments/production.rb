fdce4d44247b79a79af1b2565d5cb1198bd4144d
Elmer <efe_santos@hotmail.com>
2016-03-22 14:13:18 +0000

    Change precompile config to false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ ActionMailer::Base.delivery_method = :smtp
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
